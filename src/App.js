ff0fbd25d9b5a1d87ccc1db9876ba643f6ee00b4
原法仓 <441102979@qq.com>
2022-05-25 17:44:07 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import { ConfigProvider } from 'antd'
+import RouteWithSubRoutes from './router/RouteWithSubRoutes'
+import 'moment/locale/zh-cn'
+import zhCN from 'antd/lib/locale/zh_CN'
+import routes from './router/routerConfig'
+
+
+
 
 function App() {
+  
+console.log(routes)
+  // const debounce = (func, delay, immediate) => {
+  //   // result表示返回值
+  //   let timeout, result
+
+  //   let debounced = function () {
+  //     // 存储触发当前事件的this
+  //     let _this = this
+  //     // 存储event对象
+  //     let args = arguments
+  //     clearTimeout(timeout)
+  //     // 判断是否立即执行，如果不传默认不立即执行
+  //     if (immediate) {
+  //       // 立即执行
+  //       let callNow = !timeout
+  //       timeout = setTimeout(() => {
+  //         timeout = null
+  //       }, delay)
+  //       if (callNow) result = func.apply(_this, args)
+  //     } else {
+  //       timeout = setTimeout(function () {
+  //         func.apply(_this, args)
+  //       }, delay)
+  //     }
+  //     return result
+  //   }
+  //   debounced.cancel = function () {
+  //     clearTimeout(timeout)
+  //     timeout = null
+  //   }
+  //   return debounced
+  // }
+
+  // const setHtmlSize = () => {
+  //   let width = window.innerWidth > 2770 ? 2770 : window.innerWidth
+  //   // let width = window.innerWidth 
+  //   document.documentElement.style.fontSize = `${width / 192}px`
+  // }
+
+  // useEffect(() => {
+  //   setHtmlSize()
+  //   window.addEventListener('resize', debounce(setHtmlSize, 600, false))
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider locale={zhCN}>
+      <div className="App">
+        {
+          routes && routes.length ? <RouteWithSubRoutes routes={routes} /> : null
+        }
+
+      </div>
+    </ConfigProvider>
+
   );
 }
 
